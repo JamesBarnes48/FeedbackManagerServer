ada60ac7f50a1d995ca8b3cb02891016bd6b70b9
james.barnes <james.barnes@solirius.com>
2025-05-15 17:09:56 +0100

    setup express rate limiter

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 
 //setup express
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
+
+//setup rate limiter middleware
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 10, // each IP can make up to 10 requests per minute
+    standardHeaders: 'draft-8', 
+    legacyHeaders: false,
+});
 
 //catch invalid routes
 app.all('*', (req, res) => {
