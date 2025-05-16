53fbae45138f8fa37c3f2d22a34bd3c67a95170a
james.barnes <james.barnes@solirius.com>
2025-05-16 16:21:48 +0100

    create post feedback route with validation

--- src/classes/Feedback.ts
+++ src/classes/Feedback.ts
@@ -1,3 +1,20 @@
 export default class Feedback{
-    constructor(public isPositive: boolean, public rating: number, public expectation: string, public details: string){}
+    constructor(readonly isPositive: boolean, readonly rating: number, readonly expectation: string, readonly details: string){
+        //try to infer isPositive unless unprovided
+        this.isPositive = isPositive === undefined? isPositive: !!isPositive;
+        //NaN ratings will be caught at isValid
+        this.rating = +rating;
+        this.expectation = String(expectation);
+        this.details = String(details);
+    }
+
+    readonly validExpectations = ['strongly disagree', 'disagree', 'neither agree nor disagree', 'agree', 'strongly agree'];
+
+    toString() {
+        return {"isPositive": this.isPositive, "rating": this.rating, "expectation": this.expectation, "details": this.details}
+    }
+
+    isValid() {
+        return !!(this.rating > 0 && this.rating <= 5) && this.validExpectations.includes(this.expectation) && this.details.length;
+    }
 }
--- src/routes/feedback.router.ts
+++ src/routes/feedback.router.ts
@@ -1,8 +1,22 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import Feedback from '../classes/Feedback'
 
 export const feedbackRouter = express.Router();
 feedbackRouter.use(express.json());
 
 feedbackRouter.get('/', (req, res) => {
     res.send('feedback!');
+})
+
+feedbackRouter.post('/', (req: Request, res: Response) => {
+    try{
+        if(!req.body?.feedbackProps) res.status(400).send('feedbackProps could not be found in request body');
+        const {isPositive, rating, expectation, details} = req.body.feedbackProps;
+    
+        const feedbackInstance = new Feedback(isPositive, rating, expectation, details);
+        if(!feedbackInstance.isValid()) res.status(400).send('Invalid feedback parameters');
+        res.status(200).send('success');
+    }catch(err){
+        res.status(500).send('Something went wrong');
+    }
 })
