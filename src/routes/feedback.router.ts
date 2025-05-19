4ace5a8987dea3527c0c0f8a47baa8a23910b4fa
james.barnes <james.barnes@solirius.com>
2025-05-19 13:35:13 +0100

    add delete endpoint

--- src/routes/feedback.router.ts
+++ src/routes/feedback.router.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 
 import { collections } from "../connection";
 import Feedback from '../classes/Feedback'
+import { ObjectId } from 'mongodb';
 
 export const feedbackRouter = express.Router();
 feedbackRouter.use(express.json());
@@ -9,8 +10,7 @@ feedbackRouter.use(express.json());
 feedbackRouter.get('/', async (req: Request, res: Response) => {
     try{
         const result = await collections.feedback!.find().toArray();
-        const feedbacks: Object[] = result.map((r) => {return {id: r._id, isPositive: r.isPositive, rating: r.rating, expectation: r.expectation, details: r.details}});
-        
+        const feedbacks = result.map((r) => {return {id: r._id, isPositive: r.isPositive, rating: r.rating, expectation: r.expectation, details: r.details}});
         res.status(200).json({feedbacks});
     }catch(err){
         console.error('GET /feedback error: ', err);
@@ -42,4 +42,21 @@ feedbackRouter.post('/', async (req: Request, res: Response) => {
         console.error('POST /feedback error: ', err);
         res.status(500).send('Something went wrong');
     }
+})
+
+feedbackRouter.delete('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if(!id){
+        res.status(400).send('Invalid parameters');
+        return;
+    }
+
+    const result = await collections.feedback!.deleteOne({_id: new ObjectId(id)})
+    if(result && result.deletedCount){
+        if(result.deletedCount) res.status(200).send('Successfully deleted feedback with id: ' + id);
+        else res.status(404).send('Feedback with id ' + id + ' does not exist');
+        return;
+    }
+    res.status(400).send('Failed to remove feedback with id: ' + id);
+    return;
 })
