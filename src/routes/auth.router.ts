385edf5f5916169cb05e9e25b364f91d3e33aaeb
james.barnes <james.barnes@solirius.com>
2025-05-27 11:05:52 +0100

    auth user creds and generate token

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express';
 import { collections } from "../connection";
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 export const authRouter = express.Router();
 authRouter.use(express.json());
@@ -8,5 +10,22 @@ authRouter.post('/login', async (req: Request, res: Response) => {
     const { username, password } = req.body;
     //check if user exists
     const foundUser = await collections.users!.findOne({username: username});
-    res.send(foundUser);
+    if(!foundUser){
+        res.status(401).json({message: 'Invalid username or password'});
+        return;
+    }
+    //use bcrypt to check that entered password matches stored password after hashing
+    const match = await bcrypt.compare(password, foundUser.password);
+    if(!match){
+        res.status(401).json({message: 'Invalid username or password'});
+        return;
+    }
+    
+    //create auth token - payload is publically visible so must not contain sensitive info
+    const token = jwt.sign(
+        { username },
+        process.env.JWT_SECRET_KEY,
+        {expiresIn: '1h'}
+    );
+    res.status(200).json({message: 'Login successful', token});
 })
