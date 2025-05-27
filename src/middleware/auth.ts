db9bbc6992075234eff9467f653a5ef74d3ec86b
james.barnes <james.barnes@solirius.com>
2025-05-27 17:29:04 +0100

    define checkLoggedIn middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,6 @@
+import {Request, Response, NextFunction} from 'express';
+const jwt = require('jsonwebtoken');
+
+export default function checkLoggedIn(req: Request, res: Response, next: NextFunction){
+    
+}
